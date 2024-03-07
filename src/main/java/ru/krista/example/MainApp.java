558b3597fad41fd6404e7b172e46f3175fb3d3b1
gatiikrashen@gmail.ru <gatiikrashen@gmail.com>
2024-03-07 09:22:28 +0300

    добавил комментарий

--- src/main/java/ru/krista/example/MainApp.java
+++ src/main/java/ru/krista/example/MainApp.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        // гладиолус
 
         while (true) {
 
